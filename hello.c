dbb881e3a8671ada62e3ed00c478cf75dd0f2ed5
Jjia5 <806833559@qq.com>
2018-08-24 11:33:29 +0800

    change

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@
 int main (int argc, char const *argv[]) {
    printf("Wendian Zhang Computer Science\n");
    printf("Linhan Li Computer Science\n");
+   printf("Jiahua Jia Computer Science\n");
    return 0;
  }
