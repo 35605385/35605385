caae5b775d77f78f5d32b1c9843d8a7e34ae70a6
SWVM <lmax.qaz@gmail.com>
2018-08-23 21:10:49 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main (int argc, char const *argv[]) {
-   printf("Wendian Zhang Computer Science\n", );
-   printf("Linhan Li Computer Science\n", );
+   printf("Wendian Zhang Computer Science\n");
+   printf("Linhan Li Computer Science\n");
    return 0;
  }
